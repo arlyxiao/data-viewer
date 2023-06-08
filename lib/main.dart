eda04c8aeecf0bfa5df6518c52b65ff8bb40c090
Arly Xiao <keeptruth@icloud.com>
2023-06-08 15:47:33 +0800

    Show listview

--- lib/main.dart
+++ lib/main.dart
@@ -37,14 +37,15 @@ class Form extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<FormModel>(builder: (context, model, child) {
-          return Column(
-            children: [
-              Text('${model.counter}'),
-              TextButton(onPressed: model.increase, child: const Text('increase'))
-            ],
-          );
-        });
+    return Scaffold(
+        body: Column(
+      children: [
+        Text('${context.watch<FormModel>().counter}'),
+        const SizedBox(width: 400, height: 300, child: DbConnectionForm()),
+        const SizedBox(width: 400, height: 200, child: MyWidget()),
+        TextButton(onPressed: Provider.of<FormModel>(context).increase, child: const Text('increase'))
+      ],
+    ));
   }
 }
 
@@ -61,5 +62,79 @@ class MyApp extends StatelessWidget {
         routerConfig: router(),
       ),
     );
+  }
+}
+
+class DbConnectionForm extends StatefulWidget {
+  const DbConnectionForm({super.key});
+
+  @override
+  State<DbConnectionForm> createState() => _DbConnectionFormState();
+}
+
+class _DbConnectionFormState extends State<DbConnectionForm> {
+  var _type = '';
+  var _host = '';
+  var _username = '';
+  var _password = '';
+
+  void _connect() {
+    Provider.of<FormModel>(context, listen: false).insert(type: _type, host: _host, username: _username, password: _password);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          onChanged: (text) {
+            setState(() {
+              _type = text;
+            });
+          },
+        ),
+        TextField(
+          onChanged: (text) {
+            setState(() {
+              _host = text;
+            });
+          },
+        ),
+        TextField(
+          onChanged: (text) {
+            setState(() {
+              _username = text;
+            });
+          },
+        ),
+        TextField(
+          onChanged: (text) {
+            setState(() {
+              _password = text;
+            });
+          },
+        ),
+        TextButton(onPressed: _connect, child: const Text('Connect'))
+      ],
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var entries = context.watch<FormModel>().dbList['mysql'];
+
+    return ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: entries!.length,
+        itemBuilder: (BuildContext context, int index) {
+          return SizedBox(
+            height: 50,
+            child: Center(child: Text('Entry ${entries[index]}')),
+          );
+        });
   }
 }
--- lib/models/form.dart
+++ lib/models/form.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/foundation.dart';
 
 class FormModel extends ChangeNotifier {
-  int _counter = 0;
+  final dbTypes = ['mysql', 'postgres'];
+  var dbList = {
+    'mysql': [],
+    'postgres': [],
+  };
+  var person = {'counter': 0};
 
-  int get counter => _counter;
+  int get counter => person['counter']!;
 
-  increase() {
-    _counter += 1;
+  void insert({required String type, required String host, required String username, required String password}) {
+    if (!dbTypes.contains(type)) {
+      return;
+    }
+    
+    dbList[type]!.add({'host': host, 'username': username, 'password': password});
+
+    notifyListeners();
+  }
+
+  void increase() {
+    person['counter'] = person['counter']! + 1;
 
     notifyListeners();
   }
